5c49d236bce2d8d425cef36b82777ef8501e6d43
Jan-Olof Nilsson <janolof@gmail.com>
2017-02-16 12:36:27 +0100

    Added EF core test project.

---
+++ EFGetStarted.AspNetCore.NewDb/Models/Model.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace EFGetStarted.AspNetCore.NewDb.Models
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public List<Post> Posts { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class BloggingContext : DbContext
+    {
+        public BloggingContext(DbContextOptions<BloggingContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public Blog Blog { get; set; }
+        public int BlogId { get; set; }
+        public string Content { get; set; }
+        public int PostId { get; set; }
+        public string Title { get; set; }
+    }
+}
