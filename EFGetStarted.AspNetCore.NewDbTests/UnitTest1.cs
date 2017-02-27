fb15266c3628e4e064d62004cb3a3086d577f74e
Jan-Olof Nilsson <janolof@gmail.com>
2017-02-27 10:21:14 +0100

    Added db tests.

---
+++ EFGetStarted.AspNetCore.NewDbTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EFGetStarted.AspNetCore.NewDbTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
